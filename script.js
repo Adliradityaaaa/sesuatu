48432ff2a17aac6d8b6ca82e1183eb67de15297a
Ghujuj <m.adliradityar@gmail.com>
2024-09-28 01:39:23 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+function revealMessage() {
+  const secretMessage = document.getElementById('secret-message');
+  secretMessage.textContent = "aku minta maaf yaa semoga kita bisa sama sama lagi nanti";
+  secretMessage.classList.add('show');
+}
